2c0fd9a869a26c5617b8793b12f5deafabdf2651
zitongcharliedeng <zitongcharliedeng2003@gmail.com>
2023-11-17 21:38:19 +0000

    Add broken tests

---
+++ src/test/java/com/makersacademy/acebook/model/AppTest.java
@@ -0,0 +1,52 @@
+package com.makersacademy.acebook.model;
+
+import com.github.javafaker.Faker;
+import com.makersacademy.acebook.Application;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+public class AppTest {
+
+    WebDriver driver;
+    Faker faker;
+
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        driver = new ChromeDriver();
+        faker = new Faker();
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+
+
+    @Test
+    public void GET_loginRedirectsToUsersNewPage() {
+        driver.get("http://localhost:8080/login");
+        String title = driver.getTitle();
+        Assert.assertEquals("Please sign in", title);
+    }
+    @Test
+    public void registrationButtonPresentOnLoginPage() {
+        driver.get("http://localhost:8080/users/new");
+        driver.findElement(By.id("username")).sendKeys(faker.name().firstName());
+        driver.findElement(By.id("password")).sendKeys("password");
+        driver.findElement(By.id("submit")).click();
+        String title = driver.getTitle();
+        Assert.assertEquals("Please sign in", title);
+    }
+}
